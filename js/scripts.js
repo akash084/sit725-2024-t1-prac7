5e457791adad01fbf474405dab36a4c76504d980
akash084 <akashbaniya084@gmail.com>
2024-04-14 16:30:48 +1000

    Fourth commit
    Working full functional form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,64 @@
-// const { postCard } = require("./server.js");
-
 const addCards = (items) => {
-	arr = Array.from(items);
-	arr.forEach((item) => {
+	items.forEach((item) => {
 		let itemToAppend =
 			'<div class="col s4 center-align">' +
 			'<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
 			item.path +
-			'"></div>' +
-			'<div class="card-content">' +
+			'">' +
+			'</div><div class="card-content">' +
 			'<span class="card-title activator grey-text text-darken-4">' +
 			item.title +
-			'<i class="material-icons right">more_vert</i></span><p><a href="#"></a></p></div>' +
+			'<i class="material-icons right">more_vert</i></span><p><a href="#">' +
+			item.link +
+			"</a></p></div>" +
 			'<div class="card-reveal">' +
 			'<span class="card-title grey-text text-darken-4">' +
-			item.subTitle +
+			item.title +
 			'<i class="material-icons right">close</i></span>' +
 			'<p class="card-text">' +
-			item.description +
+			item.desciption +
 			"</p>" +
 			"</div></div></div>";
 		$("#card-section").append(itemToAppend);
 	});
 };
 
-const submitForm = () => {
-	let formData = {
-		title: $("#title").val(),
-		subTitle: $("#subTitle").val(),
-		path: $("#path").val(),
-		description: $("#description").val(),
-	};
+function submitForm() {
+	let formData = {};
+	formData.title = $("#title").val();
+	formData.path = $("#path").val();
+	formData.link = $("#link").val();
+	formData.description = $("#description").val();
 
-	console.log(formData);
-	// postCard(formData);
-};
+	console.log("Form Data Submitted: ", formData);
+	postCat(formData);
+}
 
-const getcards = () => {
-	$.get("/api/cards", (response) => {
-		if (response.statusCode == 200) {
-			console.log(response.data);
+function getCats() {
+	$.get("api/cats", (response) => {
+		console.log(response.data);
+		if (response.data) {
 			addCards(response.data);
 		}
 	});
-};
+}
+
+function postCat(cat) {
+	$.ajax({
+		url: "api/cat",
+		data: cat,
+		type: "POST",
+		success: (result) => {
+			console.log(result.data);
+		},
+	});
+}
 
 $(document).ready(function () {
 	$(".materialboxed").materialbox();
+	getCats();
+	$(".modal").modal();
 	$("#formSubmit").click(() => {
 		submitForm();
 	});
-
-	$(".modal").modal();
-	getcards();
 });
--- server.js
+++ server.js
@@ -1,76 +1,161 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const app = express();
+let express = require("express");
+let app = express();
+let port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
 	"mongodb+srv://akashbaniya084:DM5lOlHA5eEl94Bd@cluster0.4edcxj4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const port = process.env.PORT || 3000;
-let collection;
-
-app.use(express.static(__dirname + "/"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 const client = new MongoClient(uri, {
 	serverApi: {
-		version: "1",
+		version: ServerApiVersion.v1,
 		strict: true,
 		deprecationErrors: true,
 	},
 });
 
-async function runDBConnection() {
+async function run() {
 	try {
 		await client.connect();
-		collection = client.db().collection("Cat");
-		console.log(collection);
-	} catch (ex) {
-		console.error(ex);
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		await client.close();
 	}
 }
 
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
-});
-
-const getAllCats = async () => {
-	try {
-		const cats = await collection.find({}).toArray();
-		return cats;
-	} catch (err) {
-		throw new Error("Error fetching cats");
-	}
-};
-
-app.get("/api/cards", async (req, res) => {
-	try {
-		const result = await getAllCats();
-		res.json({
-			statusCode: 200,
-			data: result,
-			message: "get all the cards success",
-		});
-	} catch (err) {
-		res.status(500).json({
-			statusCode: 500,
-			message: "Internal server error",
-			error: err.message,
-		});
-	}
-});
-
-const postCard = (formData) => {
-	console.log(formData);
-};
+app.use(express.static(__dirname + "/"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	if (req.statusCode == 200) {
-		console.log("success Hurray");
-	}
+	res.render("index.html");
 });
+
+app.post("/api/cat", async (req, res) => {
+	let cat = req.body;
+	let result = await postCat(cat);
+	client.close();
+	res.json({ statusCode: 201, message: "success", data: result });
+});
+
+async function postCat(cat) {
+	await client.connect();
+	let collection = await client.db().collection("Cat");
+	return collection.insertOne(cat);
+}
+
+app.get("/api/cats", async (req, res) => {
+	let result = await getAllCats();
+	client.close();
+	res.json({ statusCode: 201, message: "success", data: result });
+});
+
+async function getAllCats() {
+	await client.connect();
+	let collection = await client.db().collection("Cat");
+	return collection.find().toArray();
+}
 
 app.listen(port, () => {
-	console.log("Express server started");
-	runDBConnection();
+	console.log("server started");
+	//run().catch(console.dir);;
 });
+
+// const express = require("express");
+// const { MongoClient } = require("mongodb");
+// const app = express();
+// const uri =
+// 	"mongodb+srv://akashbaniya084:DM5lOlHA5eEl94Bd@cluster0.4edcxj4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const port = process.env.PORT || 3000;
+// let collection;
+
+// app.use(express.static(__dirname + "/"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// const client = new MongoClient(uri, {
+// 	serverApi: {
+// 		version: "1",
+// 		strict: true,
+// 		deprecationErrors: true,
+// 	},
+// });
+
+// async function runDBConnection() {
+// 	try {
+// 		await client.connect();
+// 		collection = client.db().collection("Cat");
+// 		console.log(collection);
+// 	} catch (ex) {
+// 		console.error(ex);
+// 	}
+// }
+
+// app.get("/", (req, res) => {
+// 	res.sendFile(__dirname + "/index.html");
+// });
+
+// app.post("/api/postCard", async (req, res) => {
+// 	let card = req.body;
+// 	let result = await postCat(card);
+// 	client.close();
+// 	res.json({ statusCode: 201, message: "success", data: result });
+// });
+
+// async function postCat(card) {
+// 	await client.connect();
+// 	let collection = await client.db().collection("Cat");
+// 	return collection.insertOne(card);
+// }
+
+// const getAllCats = async () => {
+// 	try {
+// 		const cats = await collection.find({}).toArray();
+// 		return cats;
+// 	} catch (err) {
+// 		throw new Error("Error fetching cats");
+// 	}
+// };
+
+// // app.get("/api/cards", async (req, res) => {
+// // 	try {
+// // 		const result = await getAllCats();
+// // 		client.close();
+// // 		res.json({
+// // 			statusCode: 200,
+// // 			data: result,
+// // 			message: "get all the cards success",
+// // 		});
+// // 	} catch (err) {
+// // 		res.status(500).json({
+// // 			statusCode: 500,
+// // 			message: "Internal server error",
+// // 			error: err.message,
+// // 		});
+// // 	}
+// // });
+
+// app.get("/api/cards", async (req, res) => {
+// 	let result = await getAllCats();
+// 	client.close();
+// 	res.json({ statusCode: 200, message: "success", data: result });
+// });
+
+// // const postCard = (formData) => {
+// // 	console.log(formData);
+// // };
+
+// // app.get("/", (req, res) => {
+// // 	if (req.statusCode == 200) {
+// // 		console.log("success Hurray");
+// // 	}
+// // });
+
+// app.listen(port, () => {
+// 	console.log("Express server started");
+// 	runDBConnection();
+// });
 
 // module.exports = { postCard };
